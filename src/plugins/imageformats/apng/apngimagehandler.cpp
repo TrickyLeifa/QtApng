466d57799f663631ffd76fb9d3eb8d1c7ae2f2d8
TrickyLeifa <date.epoch@gmail.com>
2023-01-12 11:49:46 +0100

    Now Qt6 compliant

--- src/plugins/imageformats/apng/apngimagehandler.cpp
+++ src/plugins/imageformats/apng/apngimagehandler.cpp
@@ -2,109 +2,115 @@
 #include "apngreader_p.h"
 
 ApngImageHandler::ApngImageHandler() :
-	QImageIOHandler{},
-	_reader{new ApngReader{}}
+    QImageIOHandler{},
+    _reader{new ApngReader{}}
 {}
 
 ApngImageHandler::~ApngImageHandler() = default;
 
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
 QByteArray ApngImageHandler::name() const
 {
-	return "apng";
+    return "apng";
 }
+#endif
 
 bool ApngImageHandler::canRead() const
 {
-	auto valid = _reader->init(device());
-	if(valid)
-		setFormat(_reader->isAnimated() ? "apng" : "png");
-	return valid && _index < _reader->frames();
+    auto valid = _reader->init(device());
+    if(valid)
+        setFormat(_reader->isAnimated() ? "apng" : "png");
+    return valid && _index < _reader->frames();
 }
 
 bool ApngImageHandler::read(QImage *image)
 {
-	if(!_reader->init(device()))
-		return false;
-	*image = _reader->readFrame(_index++);
-	return !image->isNull();
+    if(!_reader->init(device()))
+        return false;
+    *image = _reader->readFrame(_index++);
+    return !image->isNull();
 }
 
 QVariant ApngImageHandler::option(QImageIOHandler::ImageOption option) const
 {
-	switch(option) {
-	case QImageIOHandler::Size:
-		if(_reader->init(device()))
-			return _reader->size();
-		else
-			return QSize();
-	case QImageIOHandler::Animation:
-		if(_reader->init(device()))
-			return _reader->isAnimated();
-		else
-			return true;
-	default:
-		return QVariant();
-	}
+    switch(option) {
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+    case QImageIOHandler::Name:
+        return QLatin1String("apng");
+#endif
+    case QImageIOHandler::Size:
+        if(_reader->init(device()))
+            return _reader->size();
+        else
+            return QSize();
+    case QImageIOHandler::Animation:
+        if(_reader->init(device()))
+            return _reader->isAnimated();
+        else
+            return true;
+    default:
+        return QVariant();
+    }
 }
 
 bool ApngImageHandler::supportsOption(QImageIOHandler::ImageOption option) const
 {
-	switch(option) {
-	case QImageIOHandler::Size:
-	case QImageIOHandler::Animation:
-		return true;
-	default:
-		return false;
-	}
+    switch(option) {
+    case QImageIOHandler::Size:
+    case QImageIOHandler::Animation:
+        return true;
+    default:
+        return false;
+    }
 }
 
 bool ApngImageHandler::jumpToNextImage()
 {
-	if(!_reader->init(device()))
-		return false;
-	else
-		return (++_index < _reader->frames());
+    if(!_reader->init(device()))
+        return false;
+    else
+        return (++_index < _reader->frames());
 }
 
 bool ApngImageHandler::jumpToImage(int imageNumber)
 {
-	if(!_reader->init(device()) || imageNumber < 0)
-		return false;
-	_index = static_cast<quint32>(imageNumber);
-	return _index < _reader->frames();
+    if(!_reader->init(device()) || imageNumber < 0)
+        return false;
+    _index = static_cast<quint32>(imageNumber);
+    return _index < _reader->frames();
 }
 
 int ApngImageHandler::loopCount() const
 {
-	if(_reader->init(device()) && _reader->isAnimated()) {
-		auto plays = _reader->plays();
-		if(plays == 0)
-			return -1;
-		else
-			return static_cast<int>(plays);
-	} else
-		return 0;
+    if(_reader->init(device()) && _reader->isAnimated()) {
+        auto plays = _reader->plays();
+        if(plays == 0)
+            return -1;
+        else
+            return static_cast<int>(plays);
+    } else
+        return 0;
 }
 
 int ApngImageHandler::imageCount() const
 {
-	if(_reader->init(device()))
-		return static_cast<int>(_reader->frames());
-	else
-		return 0;
+    if(_reader->init(device()))
+        return static_cast<int>(_reader->frames());
+    else
+        return 0;
 }
 
 int ApngImageHandler::nextImageDelay() const
 {
-	if(!_reader->init(device()))
-		return false;
-	if(_index == 0 || _index > _reader->frames())
-		return 0;
-	else
-		return _reader->readFrame(_index - 1).delayMsec();
+    if(!_reader->init(device()))
+        return false;
+    if(_index == 0 || _index > _reader->frames())
+        return 0;
+    else
+        return _reader->readFrame(_index - 1).delayMsec();
 }
 
 int ApngImageHandler::currentImageNumber() const
 {
-	return _index < _reader->frames() ? static_cast<int>(_index) : -1;
+    return _index < _reader->frames() ? static_cast<int>(_index) : -1;
 }
--- src/plugins/imageformats/apng/apngimagehandler_p.h
+++ src/plugins/imageformats/apng/apngimagehandler_p.h
@@ -9,27 +9,29 @@ class ApngReader;
 
 class ApngImageHandler : public QImageIOHandler
 {
-	Q_DISABLE_COPY(ApngImageHandler)
+    Q_DISABLE_COPY(ApngImageHandler)
 public:
-	ApngImageHandler();
-	~ApngImageHandler() override;
+    ApngImageHandler();
+    ~ApngImageHandler() override;
 
-	// QImageIOHandler interface
-	QByteArray name() const override;
-	bool canRead() const final;
-	bool read(QImage *image) final;
-	QVariant option(ImageOption option) const final;
-	bool supportsOption(ImageOption option) const final;
-	bool jumpToNextImage() final;
-	bool jumpToImage(int imageNumber) final;
-	int loopCount() const final;
-	int imageCount() const final;
-	int nextImageDelay() const final;
-	int currentImageNumber() const final;
+    // QImageIOHandler interface
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QByteArray name() const override;
+#endif
+    bool canRead() const final;
+    bool read(QImage *image) final;
+    QVariant option(ImageOption option) const final;
+    bool supportsOption(ImageOption option) const final;
+    bool jumpToNextImage() final;
+    bool jumpToImage(int imageNumber) final;
+    int loopCount() const final;
+    int imageCount() const final;
+    int nextImageDelay() const final;
+    int currentImageNumber() const final;
 
 private:
-	quint32 _index = 0;
-	QScopedPointer<ApngReader> _reader;
+    quint32 _index = 0;
+    QScopedPointer<ApngReader> _reader;
 };
 
 #endif // APNGIMAGEHANDLER_P_H
